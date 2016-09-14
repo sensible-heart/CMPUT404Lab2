4ae1ca972f8ac5876fb82ff600ee9f6997fca529
sensible-heart <alexandra.makepeace@gmail.com>
2016-09-14 15:52:54 -0600

    Allow for multiple requests without hanging.

--- lab2.py
+++ lab2.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 # in another terminal after running this type: curl 0.0.0.0:8000
 #curl 127.0.0.0.1:8000 -H "Host: www.google.ca" match header to what the server is connecting to. Host header is the only way it would know is the one we are requesting
@@ -9,39 +10,40 @@ server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 #listen on port 8000
 server.bind(('0.0.0.0', 8000))#means to listen on all addresses
 server.listen(1)
-
-print("Waiting for connections...")
-client,address = server.accept()
-print("Connected")
-print (address)
-
-#client is going to be curl, web browser or something like that
-outgoing = socket.socket()
-outgoing.connect(("www.google.ca", 80))
-outgoing.setblocking(0)#prevent indefinite waiting
-client.setblocking(0)
 while True:
-      try:
-	part = client.recv(1024)
-      except socket.error, exception:
-      	     if exception.errno == 11:
-	     	part = None
-	     else:
-		raise
-      if(part is not None and len(part) == 0):
-         exit(0)
-      if (part):
-      	 outgoing.sendall(part)
-	 print("< " + str(part))
-      try:
-	part = outgoing.recv(1024)
-      except socket.error, exception:
-      	     if exception.errno == 11:
-	     	part = None
-	     else:
-		raise
-      if(part is not None and len(part) == 0):
-             exit(0)
-      if (part):
-	 client.sendall(part)
-	 print("> " + str(part))
+      print("Waiting for connections...")
+      client,address = server.accept()
+      print("Connected")
+      print (address)
+      pid = os.fork()
+      if (pid == 0):
+            #client is going to be curl, web browser or something like that
+            outgoing = socket.socket()
+            outgoing.connect(("www.google.ca", 80))
+            outgoing.setblocking(0)#prevent indefinite waiting
+            client.setblocking(0)
+            while True:
+                  try:
+                    part = client.recv(1024)
+                  except socket.error, exception:
+                         if exception.errno == 11:
+                            part = None
+                         else:
+                            raise
+                  if(part is not None and len(part) == 0):
+                     exit(0)
+                  if (part):
+                     outgoing.sendall(part)
+                     print("< " + str(part))
+                  try:
+                    part = outgoing.recv(1024)
+                  except socket.error, exception:
+                         if exception.errno == 11:
+                            part = None
+                         else:
+                            raise
+                  if(part is not None and len(part) == 0):
+                         exit(0)
+                  if (part):
+                     client.sendall(part)
+                     print("> " + str(part))
